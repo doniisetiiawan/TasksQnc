24d9e728dcc4bcbf2a9d6ddb2f04b7cf8e70def3
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-03 13:09:26 +0700

    TextInput

--- app/components/TasksList/index.jsx
+++ app/components/TasksList/index.jsx
@@ -1,28 +1,62 @@
+/* eslint-disable react/no-access-state-in-setstate */
 import React, { Component } from 'react';
-import { FlatList, Text } from 'react-native';
-
-const DATA = [
-  'Buy milk',
-  'Walk the dog',
-  'Do laundry',
-  'Write the first chapter of my book',
-];
+import {
+  FlatList,
+  Text,
+  View,
+  TextInput,
+} from 'react-native';
+import styles from './styles';
 
 export default class TasksList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dataSource: DATA,
+      listOfTasks: [],
+      text: '',
     };
   }
 
+  _addTask = () => {
+    const newListOfTasks = [...this.state.listOfTasks, this.state.text];
+
+    this.setState({
+      listOfTasks: newListOfTasks,
+    });
+
+    this._changeTextInputValue('');
+  };
+
+  _changeTextInputValue = (text) => {
+    this.setState({
+      text,
+    });
+  };
+
+  _renderRowData = (rowData) => (
+    <Text>{rowData}</Text>
+  );
+
   render() {
+    const dataSource = this.state.listOfTasks;
+
     return (
-      <FlatList
-        data={this.state.dataSource}
-        renderItem={({ item }) => <Text>{item}</Text>}
-        keyExtractor={({ item }) => item}
-      />
+      <View style={styles.container}>
+        <TextInput
+          autoCorrect={false}
+          onChangeText={(text) => this._changeTextInputValue(text)}
+          onSubmitEditing={() => this._addTask()}
+          returnKeyType="done"
+          style={styles.textInput}
+          value={this.state.text}
+        />
+        <FlatList
+          enableEmptySections
+          data={dataSource}
+          renderItem={({ item }) => this._renderRowData(item)}
+          keyExtractor={({ item }) => item}
+        />
+      </View>
     );
   }
 }
